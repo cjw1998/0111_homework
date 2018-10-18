70f587b165fc372cc6cbedd328bda376d3534d0e
cjw1998 <36588083+cjw1998@users.noreply.github.com>
2018-10-18 10:00:26 +0800

    de

--- homework02/main.cpp
+++ homework02/main.cpp
@@ -5,46 +5,7 @@
 #include <QString>
 #include <QCoreApplication>
 
-//namespace SK {
-//enum SortKind{
-//    col01    =   0x00000001<<0,         //!< 第1列
-//    col02    =   0x00000001<<1,         //!< 第2列
-//    col03    =   0x00000001<<2,         //!< 第3列
-//    col04    =   0x00000001<<3,         //!< 第4列
-//    col05    =   0x00000001<<4,         //!< 第5列
-//    col06    =   0x00000001<<5,         //!< 第6列
-//    col07    =   0x00000001<<6,         //!< 第7列
-//    col08    =   0x00000001<<7,         //!< 第8列
-//    col09    =   0x00000001<<8,         //!< 第9列
-//    col10    =   0x00000001<<9,         //!< 第10列
-//    col11    =   0x00000001<<10,        //!< 第11列
-//    col12    =   0x00000001<<11,        //!< 第12列
-//    col13    =   0x00000001<<12,        //!< 第13列
-//    col14    =   0x00000001<<13,        //!< 第14列
-//    col15    =   0x00000001<<14,        //!< 第15列
-//    col16    =   0x00000001<<15,        //!< 第16列
-//    col17    =   0x00000001<<16,        //!< 第17列
-//    col18    =   0x00000001<<17,        //!< 第18列
-//    col19    =   0x00000001<<18,        //!< 第19列
-//    col20    =   0x00000001<<19,        //!< 第20列
-//    col21    =   0x00000001<<20,        //!< 第21列
-//    col22    =   0x00000001<<21,        //!< 第22列
-//    col23    =   0x00000001<<22,        //!< 第23列
-//    col24    =   0x00000001<<23,        //!< 第24列
-//    col25    =   0x00000001<<24,        //!< 第25列
-//    col26    =   0x00000001<<25,        //!< 第26列
-//    col27    =   0x00000001<<26,        //!< 第27列
-//    col28    =   0x00000001<<27,        //!< 第28列
-//    col29    =   0x00000001<<28,        //!< 第29列
-//    col30    =   0x00000001<<29,        //!< 第30列
-//    col31    =   0x00000001<<30,        //!< 第31列
-//    col32    =   0x00000001<<31,        //!< 第32列
-//};
-//}
-
-
 struct studData{
-    // 请补全结构定义
 private:
     QString id;
     QString name;
@@ -66,18 +27,6 @@ QDebug operator << (QDebug d,const studData &data){
     return d;
 }
 
-//studData::setid(QString data){
-//    this->id=data;
-//}
-
-//studData::setname(QString data){
-//    this->name=data;
-//}
-
-//studData::setsubj(QString data){
-//    this->subj=data;
-//}
-
 // 比较类，用于std::sort第三个参数
 class myCmp {
 public:
@@ -98,10 +47,6 @@ bool myCmp::operator()(const studData &d1, const studData &d2)
     case 4 : return d1.subj.section("    ",3,3)>d2.subj.section("    ",3,3);
     case 5 : return d1.subj.section("    ",4,4)>d2.subj.section("    ",4,4);
     case 6 : return d1.subj.section("    ",5,5)>d2.subj.section("    ",5,5);
-    // ...
-    // 请补全运算符重载函数
-    // ...
-    // ...
     }
     return result;
 
@@ -112,9 +57,6 @@ class ScoreSorter
 {
 public:
     ScoreSorter(QString dataFile);
-    // ...
-    // 请补全该类，使其实现上述要求
-    // ...
     void readFile();
     void doSort(int j);
     void myMessageOutput();
@@ -125,7 +67,6 @@ private:
     QString title;
 };
 
-// 请补全
 ScoreSorter::ScoreSorter(QString dataFile)
 {
     this->Filepath=dataFile;
@@ -148,11 +89,7 @@ void ScoreSorter::readFile(){
         stu.name=str.section("    ",1,1);;
         stu.subj=str.section("    ",2,7);
         stuList<<stu;
-        //qDebug()<<stu;
-
     }
-    //qDebug()<<stuList;
-
 }
 
 void ScoreSorter::doSort(int j){
@@ -171,16 +108,12 @@ void ScoreSorter::doSort(int j){
     file.open(QIODevice::ReadWrite | QIODevice::Append);
     QTextStream output(&file);
     output.setCodec("UTF-8");  //编码方式
-//    stream<<QString("排序后输出，当前排序第 ")<<lie <<QString(" 列：")<<"\r\n";
-//    stream<<"\t";  //输出表头
     int num=stuList.size();
     output<<title;
     for(int i=0; i<num;i++){
-        //s=stuList[i];
         output<<stuList[i].id<<stuList[i].name<<"\t\t"<<stuList[i].subj;
         output<<"\r\n";
     }
-//    stream<<"----------------------------------------------------------------------------------------"<<"\r\n\r\n";
     file.close();
 }
 
